83c76a628dc1eb9aac757b5bb734ab9871f8166f
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-12-27 13:08:50 +0500

    react ref

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,30 +1,52 @@
 
-import { Component, useRef, useState } from "react";
+import { Component, createRef, useRef, useState } from "react";
 
-
-export const Dashboard = (props) => {
-   const ref = useRef(null)
+export const Dashboard = () => {
+    console.log('render');
+    const ref = useRef(null)
+    const array = ['q', 'w', 'e', 'r']
+    const array1 = [5, 8, 130, 44];
+    const refs = useRef(null)
     return (
         <>
-            <h1>{props.title.username}</h1>
-            <h1>{props.title.password}</h1>
+            <button className="btn"
+                ref={ref}
+                onClick={() => {
+                    console.log(ref.current);
+                    if (ref.current.textContent == 'New content') {
+                        ref.current.textContent = 'Click Me'
+
+                    } else if (ref.current.textContent = 'Click Me') {
+                        ref.current.textContent = 'New content'
+                    }
+                }}>Button</button>
+            <button disabled ref={ref}>BTN1</button>
+            <button disabled ref={ref}>BTN2</button>
+            <button disabled ref={ref}>BTN3</button>
+            <button disabled ref={ref}>BTN4</button>
+            <button disabled ref={ref}>BTN5</button>
+            <button ref={refs} onClick={() => {
+                const found = array.find((e,i) => i == 2);
+
+                console.log(found);
+            }} >BTN6</button>
+
+
+
         </>
 
     )
 }
 // export class Dashboard extends Component {
-//     state= {
-//         username: 'User'
-//     }
+//     ref = createRef();
 //     render() {
 //         return (
 //             <>
-//                 <h1>{this.props.title.username}</h1>
-//                 <h1>{this.props.title.password}</h1>
-//                 <input type="text" value={this.state.username} />
-//                 <button type="button" onClick={() => {
-//                   this.setState({username:'New User'})
-//                 }} >Click Me</button>
+//                 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. At ex nostrum laboriosam labore exercitationem, repudiandae sed dolor, unde non animi voluptatem architecto ut ducimus quisquam vel, natus nobis incidunt voluptatibus.</h1>
+//                 <button ref={this.ref} onClick={()=>{
+//                     this.ref.current.textContent = 'Click Me'
+//                     console.log(this.ref.current.textContent);
+//                 }}>REF</button>
 //             </>
 
 //         )
